1c07f30894beb46bb2b6227b9ee47fd0799eac4a
hemuba <devicentialessandro@libero.it>
2025-05-02 23:15:28 +0300

    ENH: adding exceptions to the ControllerAdvice

--- src/main/java/com/stockmanager/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stockmanager/backend/exception/GlobalExceptionHandler.java
@@ -1,14 +1,35 @@
 package com.stockmanager.backend.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handlerIllegalArgException(IllegalArgumentException exception){
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    public ResponseEntity<Map<String, String>> handlerIllegalArgException(IllegalArgumentException exception){
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", exception.getMessage());
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerValidationErrors(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach( e -> errors.put(e.getField(), e.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handlerValidationErrors(ConstraintViolationException exception){
+        Map<String, String> errors = new HashMap<>();
+        exception.getConstraintViolations().forEach(e -> errors.put(e.getPropertyPath().toString(),  e.getMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 }

10228257c084b7a40855bd95ac1d196e9b75865d
hemuba <125600353+hemuba@users.noreply.github.com>
2025-05-03 12:52:40 +0300

    Fresh branch to exercise and rebuild the project in different flavours

--- src/main/java/com/stockmanager/backend/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.stockmanager.backend.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
-}

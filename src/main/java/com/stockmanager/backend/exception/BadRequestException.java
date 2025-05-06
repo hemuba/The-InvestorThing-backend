9e8702c555d7a45f11781aba1fb0f0eae5151368
hemuba <125600353+hemuba@users.noreply.github.com>
2025-05-07 00:01:50 +0300

    MERGE master into reset-dev

--- src/main/java/com/stockmanager/backend/exception/BadRequestException.java
+++ src/main/java/com/stockmanager/backend/exception/BadRequestException.java
@@ -1,4 +1,4 @@
-package com.stockmanager.backend.exceptions;
+package com.stockmanager.backend.exception;
 
 public class BadRequestException extends RuntimeException {
 
--- src/main/java/com/stockmanager/backend/exception/NotFoundException.java
+++ src/main/java/com/stockmanager/backend/exception/NotFoundException.java
@@ -1,4 +1,4 @@
-package com.stockmanager.backend.exceptions;
+package com.stockmanager.backend.exception;
 
 public class NotFoundException extends RuntimeException{
     public NotFoundException(String message){

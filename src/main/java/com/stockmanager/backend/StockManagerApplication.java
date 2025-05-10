d79f263b9bc15b41c8697b7ab96ffe0092942161
hemuba <125600353+hemuba@users.noreply.github.com>
2025-05-10 19:13:43 +0300

    added postconstruct on springbootapplication

--- src/main/java/com/stockmanager/backend/StockManagerApplication.java
+++ src/main/java/com/stockmanager/backend/StockManagerApplication.java
@@ -1,15 +1,37 @@
 package com.stockmanager.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class
 StockManagerApplication {
+    private final Logger logger = LoggerFactory.getLogger(StockManagerApplication.class);
+
+    @Value(("${stockmanager.author}"))
+    private String author;
+
+    @Value("${stockmanager.version}")
+    private String version;
+
+    @Value("${stockmanager.module}")
+    private String module;
+
     public StockManagerApplication() {
+
+
     }
 
     public static void main(String[] args) {
         SpringApplication.run(StockManagerApplication.class, args);
     }
+    @PostConstruct
+    private void postConstruct(){
+        logger.info("Application info - Module: {} | Version: {} | Author: {}", module, version, author);
+    }
+
 }
